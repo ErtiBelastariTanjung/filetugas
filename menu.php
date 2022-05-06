83298e911dadfc61750c04b1bca4a2aedadf2888
ErtiBelastariTanjung <56757538+ErtiBelastariTanjung@users.noreply.github.com>
2022-05-06 12:31:41 +0700

    TUGAS1_MODULARITAS
    NAMA : ERTI BELASTARI TANJUNG
    NIM     : 0701192041
    KELAS  : Ilmu Komputer_3
    B.STUDI: PEMROGRAMAN WEB

---
+++ menu.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "_//W3C//DTD XHTML 1.0 Transitional//EN"  "http:// 
+www.w3.org/TR/xhtm1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Modularitas</title>
+</head>
+
+<body>
+<a href="index.php">Home</a> |
+<a href="about.php">About Us</a> |
+<a href="link.php">Links</a> |
+<a href="Contact.php">Contact</a> |
+<a href="menu.php">New Menu</a> |
+
+</body>
+</html>
